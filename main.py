8269f84c4eeb4d77535bc5b1f8d1c185515fb069
kelibarrera517-code <kelibarrera517@gmail.com>
2026-05-13 02:33:40 +0000

    agregar validacion de promedio

--- main.py
+++ main.py
@@ -9,3 +9,37 @@ while True:
     print("Escriba las 5 notas del estudiante:")
     for i in range(5):
         nota = input("Digite una nota: ")
+
+        while nota == "a" or nota == "b" or nota == "c" or nota == "d" or nota == "e" or nota == "f" or nota == "g" or nota == "h" or nota == "i" or nota == "j" or nota == "A" or nota == "B" or nota == "C" or nota == "D" or nota == "E" or nota == "F" or nota == "G" or nota == "H" or nota == "I" or nota == "J":
+
+            print("Error, la nota no puede ser una letra")
+            nota = input("Escriba una nota: ")
+
+        nota = float(nota)
+
+        while nota > 5:
+            print("Error, la nota no puede ser mayor que 5")
+            nota = float(input("Escriba una nota: "))
+
+        while nota < 0:
+            print("Error, la nota no puede ser menor que 0")
+            nota = float(input("Escriba una nota: "))
+
+        suma = suma + nota
+
+    promedio = suma / 5
+
+    print("Estudiante:", nombre)
+    print("Promedio:", promedio)
+
+    if promedio == 5:
+        print("Excelente")
+
+    elif promedio >= 4:
+        print("Aprobó")
+
+    elif promedio >= 3:
+        print("Reprobó")
+
+    else:
+        print("Reprobó")
